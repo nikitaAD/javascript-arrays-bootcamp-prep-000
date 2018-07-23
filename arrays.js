b3edee5ad4d60d5295b557a6ff79ddab016f2c7c
NIKITA AD <nikita8ad@gmail.com>
2018-07-23 05:31:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToEndOfArray()
+function addElementToEndOfArray(array, element){
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  
+}
